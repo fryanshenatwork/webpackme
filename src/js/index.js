0462db2db3fe7aaa3f957f5b3a8eb5ef2eb83100
FR <fryanshenatwork@gmail.com>
2020-04-29 00:33:02 +0800

    feat(scss) - toggler form-loading - js preferColorScheme updater

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 console.log('WebpackMe is working')
+
+const updatePreferColorSchema = function(event) {
+  const button = $(event.submitter)
+  const selectedVal = button.val()
+
+  $('body').attr('data-prefers-color', selectedVal)
+}
+window.updatePreferColorSchema = updatePreferColorSchema
